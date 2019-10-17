dec4404a8e6a859f883d6af622f3f30397b8762f
Gabriel Patrola <almeidagabrielaugusto@gmail.com>
2019-10-17 13:36:16 -0400

    finish

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ function mod(dividendo, divisor) {
 
 function gerarCPF() {
     comPontos = false; // TRUE para ativar e FALSE para desativar a pontuação.
-
     var n = 9;
     var n1 = randomiza(n);
     var n2 = randomiza(n);
@@ -52,6 +51,7 @@ function gerarCPF() {
         page.waitForNavigation(), // The promise resolves after navigation has finished
         page.click('input[type="image"][name="Submit"]') // Clicking the link will indirectly cause a navigation
     ]);
+
     const rainInput = await page.$('#fcadastro1')
     await rainInput.click({ clickCount: 3 })
     await rainInput.type(amount);
@@ -60,29 +60,66 @@ function gerarCPF() {
     await page.type('#email', 'TESTE2@TESTE2.COM');
     await page.type('#celular', '222222222');
 
-
     await page.click('input[type="image"][name="Submit"]');
     await page.screenshot({ path: 'example.png' });
-    // expose the function
-    // await page.addScriptTag({ path: 'script.js' });
     const query = "CLIQUE AQUI";
 
     page.evaluate(query => {
         const elements = [...document.querySelectorAll('a')];
-
-        // Either use .find or .filter, comment one of these
-        // find element with find
         const targetElement = elements.find(e => e.innerText.includes(query));
-
-        // OR, find element with filter
-        // const targetElement = elements.filter(e => e.innerText.includes(query))[0];
-
-        // make sure the element exists, and only then click it
         targetElement && targetElement.click();
     }, query)
-    // use page.select
-    //await page.waitForSelector('select[name="f1"]')
-    // await page.select('#f1', '5');
+
+    const selectElem = await page.$('select[name="f1"]');
+    await selectElem.type('5');
+    await page.click('input[type="image"][name="Submit"]');
+
+    page.evaluate(query => {
+        const elements = [...document.querySelectorAll('a')];
+        const targetElement = elements.find(e => e.innerText.includes(query));
+        targetElement && targetElement.click();
+    }, query)
+
+    const selectElem = await page.$('select[name="f1"]');
+    await selectElem.type('5');
+    await page.click('input[type="image"][name="Submit"]');
+
+    page.evaluate(query => {
+        const elements = [...document.querySelectorAll('a')];
+        const targetElement = elements.find(e => e.innerText.includes(query));
+        targetElement && targetElement.click();
+    }, query)
+
+    const selectElem = await page.$('select[name="f1"]');
+    await selectElem.type('5');
+    await page.click('input[type="image"][name="Submit"]');
+
+    page.evaluate(query => {
+        const elements = [...document.querySelectorAll('a')];
+        const targetElement = elements.find(e => e.innerText.includes(query));
+        targetElement && targetElement.click();
+    }, query)
+
+    const selectElem = await page.$('select[name="f1"]');
+    await selectElem.type('5');
+    await page.click('input[type="image"][name="Submit"]');
+
+    page.evaluate(query => {
+        const elements = [...document.querySelectorAll('a')];
+        const targetElement = elements.find(e => e.innerText.includes(query));
+        targetElement && targetElement.click();
+    }, query)
+
+    const selectElem = await page.$('select[name="f1"]');
+    await selectElem.type('5');
+    await page.click('input[type="image"][name="Submit"]');
+
+    page.evaluate(query => {
+        const elements = [...document.querySelectorAll('a')];
+        const targetElement = elements.find(e => e.innerText.includes(query));
+        targetElement && targetElement.click();
+    }, query)
+
     const selectElem = await page.$('select[name="f1"]');
     await selectElem.type('5');
     await page.click('input[type="image"][name="Submit"]');
